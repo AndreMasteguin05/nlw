4d9f9745b8463f0052b53f89b6b5f06cfb22cea5
AndreMasteguin05 <andre.masteguin@gmail.com>
2020-06-16 23:44:56 -0300

    finalizando projeto

---
+++ README.md
@@ -0,0 +1,7 @@
+Tecnologias
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+Node.js
+Express
+SQLite
+Nunjucks
---
+++ src/server.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const server = express()
+
+//pegar o banco de dados
+const db = require('./database/db')
+
+//configurar pasta publica
+server.use(express.static('public'))
+
+//habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({extended: true}))
+
+//utilizando template engine
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+  express: server,
+  noCache: true,
+})
+
+server.get('/', (req, res) => {
+  return res.render('index.html')
+})
+
+server.get('/create-point', (req, res) => {
+  return res.render('create-point.html')
+})
+
+server.post('/savepoint', (req, res) => {
+  console.log(req.body)
+  const query = `
+    INSERT INTO places(
+      image,
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+    ) VALUES(?,?,?,?,?,?,?)
+  `
+
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items,
+  ]
+
+  function afterInsertData(err) {
+    if (err) {
+      console.log(err)
+      return res.send('Erro no cadastro')
+    }
+
+    console.log('Cadastrado com sucesso')
+    console.log(this)
+  }
+  db.run(query, values, afterInsertData)
+  return res.render('create-point.html', {saved: true})
+})
+
+server.get('/search', (req, res) => {
+  const search = req.query.search
+
+  if (search === '') {
+    return res.render('search-results.html', {total: 0})
+  }
+
+  //pegar os dados do banco de dados
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(
+    error,
+    rows
+  ) {
+    if (error) {
+      return console.log(error)
+    }
+
+    const total = rows.length
+    return res.render('search-results.html', {places: rows, total: total})
+  })
+})
+
+server.listen(3000)
